8981da517db545fb57d7aaa33054a1c6aab0660a
“Anne <Nkone Gaitate>
2023-04-18 12:17:09 -0700

    Test file

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - This function calls the function simple_shell
+ * It takes no arguments
+ * Return: 0 on success error otherwise
+ */
+
 int main()
 {
 	void simple_shell(void);
