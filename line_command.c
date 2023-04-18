8f99b5c322f77ba546a7438a7a2886f91f42b3db
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-04-18 06:15:02 -0700

    first corrections

--- line_command.c
+++ line_command.c
@@ -4,25 +4,26 @@
 /**
  * main - This program writes a UNIX command line interpreter
  * Takes no arguments
- * return: 0 when the program is successful
+ * Return: 0 when the program is successful
+ */
 
-int main()
+int main(void)
 {
 	char *inpts[MAX_COMMAND_LENGTH];
 	char cmd[MAX_COMMAND_LENGTH];
-	char *tkn
+	char *tkn;
 
 	int i = 0;
 
 	while (1)
 	{
 		printf("#");
-		
+
 		if (fgets(cmd, MAX_COMMAND_LENGTH, stdin) == NULL)
 		{
 			printf("End of line");
 		}
-	
+
 		cmd[strcspn(cmd, "\n")] = '\0';
 
 		tkn = strtok(cmd, " ");
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - main point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success error otherwise
+ */
+int main(int argc, char **argv)
+{
+
+	return (0);
+}
