3dfccb5d5df6788309b07bf47d1aedf84406790c
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-04-19 08:41:33 -0700

    Task 2 handle command

--- 2-handle.c
+++ 2-handle.c
@@ -1,24 +1,32 @@
 #include "main.h"
 /**
  * handle_argument - function that handle command line argument
- * @argc: argument charecter
+ * @argc: argument count
  * @argv: argument victor
  * Return: 0 for success
  */
 
-int handle_argument(int argc, char *argv)
+int handle_argument(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0 : i < argc[] ; i++)
+	for (i = 0 ; i < argc ; i++)
 	{
-		printf("argument %d: %s\n", i, argc[i]);
+		printf("argument %d: %s\n", i, argv[i]);
 	}
+	return (0);
+
 }
-int main(int argc, char *argv[]);
+/**
+ * main - main entry point
+ * @argc: argument count
+ * @argv: argument victor
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
 {
-	handle_argument(arc, argv);
+	handle_argument(argc, argv);
 
 	return (0);
 }
-
